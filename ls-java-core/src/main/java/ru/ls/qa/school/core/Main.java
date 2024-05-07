1483c4068314b5cabe932cf8bbf4f5cac70e6b43
13th <pavel.voronin@ligastavok.ru>
2024-05-07 15:57:30 +0300

    commit 04

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(1111);
         System.out.println(2222);
         someMethod();
+        System.out.println(4444);
     }
 
     private static void someMethod() {
