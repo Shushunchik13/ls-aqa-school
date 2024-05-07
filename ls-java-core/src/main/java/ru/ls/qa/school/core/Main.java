6821a71e9cc51970170a6cc7867237bcdef61dc3
13th <pavel.voronin@ligastavok.ru>
2024-05-07 13:43:51 +0300

    commit 3

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("task 1 - done");
+
+        someMethod();
+    }
+
+    private static void someMethod() {
+        System.out.println("...");
     }
 }
