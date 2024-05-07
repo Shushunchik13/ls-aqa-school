78d37b2e30761d2b22c093ada2a12f5df14071a6
13th <pavel.voronin@ligastavok.ru>
2024-05-07 11:17:32 +0300

    change 1

--- addressbook-web-tests/src/main/java/ru/ls/qa/school/addressbook/Main.java
+++ addressbook-web-tests/src/main/java/ru/ls/qa/school/addressbook/Main.java
@@ -3,7 +3,6 @@ package ru.ls.qa.school.addressbook;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("123");
         System.out.println(234);
     }
 }
