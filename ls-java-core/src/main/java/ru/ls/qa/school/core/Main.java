2a7fe2d7af6cadd81fe43fad52122f9173a2a3e0
13th <pavel.voronin@ligastavok.ru>
2024-05-07 14:01:36 +0300

    commit 0

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -3,12 +3,5 @@ package ru.ls.qa.school.core;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("task 1 - dune");
-
-        someMethod();
-    }
-
-    private static void someMethod() {
-        System.out.println("...2");
     }
 }
