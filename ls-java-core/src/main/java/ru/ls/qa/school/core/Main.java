e22b70d6b55cbe2f71ca21e642a11100a287e64c
13th <pavel.voronin@ligastavok.ru>
2024-05-08 13:00:23 +0300

    Процесс ревью ДЗ

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -2,6 +2,6 @@ package ru.ls.qa.school.core;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Не забывайте после правок нажимать \"1request review\"");
+        System.out.println("Не забывайте после правок нажимать \"request review\"");
     }
 }
